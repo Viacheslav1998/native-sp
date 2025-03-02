e678602e7cf9accbbf0323adc1d1ac8f888f638d
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-02 23:17:09 +0500

    feat: controller, conf

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,3 +1,12 @@
 <?php 
 
 namespace Core;
+
+class Controller
+{
+    protected function render(string $view, array $data = [])
+    {
+        extract($data);
+        require __DIR__ . "/../Views/$view.php";
+    }
+}
