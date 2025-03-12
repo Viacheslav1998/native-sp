adcd5d4737b5bfc7ff3bc8df7b31adb92d6ef976
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-12 23:57:54 +0500

    feat: favicons

--- App/Views/layouts/main.php
+++ App/Views/layouts/main.php
@@ -4,7 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>тут плейсхолдер</title>
-  <link rel="stylesheet" href="<?= base_url('/root/static/main.css') ?>">
+  <link rel="icon" type="image/png" href="<?= base_url('favicon/favicon-96x96.png') ?>">
+  <link rel="icon" type="image/svg+xml" href="<?= base_url('favicon/favicon.svg') ?>">
+  <link rel="shortcut icon" href="<?= base_url('favicon/favicon.ico') ?>">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('favicon/apple-touch-icon.png') ?>">
+  <link rel="stylesheet" href="<?= base_url('root/static/main.css') ?>">
   <script type="module" src="<?= base_url('/root/static/main.js') ?>"></script>
   <script defer src="<?= base_url('/root/custom/custom.js') ?>"></script>
 </head>
