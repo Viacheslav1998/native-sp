d5bf5ef9ab2f08848b388e099609ddb1a01627e2
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-13 21:41:02 +0500

    feat: header style

--- App/Views/layouts/main.php
+++ App/Views/layouts/main.php
@@ -9,16 +9,15 @@
   <link rel="shortcut icon" href="<?= base_url('favicon/favicon.ico') ?>">
   <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('favicon/apple-touch-icon.png') ?>">
   <link rel="stylesheet" href="<?= base_url('root/static/main.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('root/custom/custom.css') ?>">
   <script type="module" src="<?= base_url('/root/static/main.js') ?>"></script>
   <script defer src="<?= base_url('/root/custom/custom.js') ?>"></script>
 </head>
 <body>
-  <div class="container bg-primary">
-
-  <h1 class="text-primary">Привет, Bootstrap!</h1>
+  <div class="container">
     <?php require_once __DIR__ . "../../parts/header.php"; ?>
 
-      <h2>а тут какой то контент</h2>
+    <h2>а тут какой то контент</h2>
 
     <?php require_once __DIR__ .  "../../parts/footer.php"; ?>
   </div>
--- App/Views/parts/header.php
+++ App/Views/parts/header.php
@@ -1,6 +1,18 @@
 <header>
-  <nav>
-    <a href="/">Главная</a> |
-    <a href="/about">О нас</a>
-  </nav>
+  <div class="logo-image p-5">
+    <h1 class="text-white">Hastle.</h1>
+  </div>
+  <div class="info-box bg-warning"> 
+    <div class="row">
+      <div class="col">быстрый контакт: 8 747-233-36-00</div>
+      <div class="col">Улица: Атмосферная 2/5, офис 216</div>
+    </div>
+    <nav>
+      <a href="/">главная</a>
+      <a href="/about">о нас</a>
+      <a href="/maps">карты</a>
+      <a href="/products">продукты</a>
+      <a href="/best">лучшее</a>
+    </nav>
+  </div>
 </header>
