66b9a16b8fe73710bc8b1885305c13de705cf172
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-14 20:41:01 +0500

    feat: css

--- App/Views/parts/header.php
+++ App/Views/parts/header.php
@@ -1,21 +1,25 @@
 <header>
   <div class="box-logo-image p-5">
-    <div class="logo align-items-start">
-      <h1 class="text-white">Hastle.</h1>
+    <div class="row logo">
+      <div class="col">
+        <h1 class="text-white">Hastle.</h1>
+      </div>
+      <div class="col text-white text-end">Улица: Атмосферная 2/5, офис 216</div>
     </div>
-    <div class="header-content align-items-end text-white">
-      <div class="info-box"> 
-        <div class="row">
-          <div class="col">быстрый контакт: 8 747-233-36-00</div>
-          <div class="col">Улица: Атмосферная 2/5, офис 216</div>
+    <div class="header-content">
+      <div class="row align-items-end info-box-height">
+        <div class="info-box"> 
+          <div class="row text-white">
+            <div class="col">быстрый контакт: 8 747-233-36-00</div>
+          </div>
+          <nav class="custom-text-nav">
+            <a href="/">главная</a>
+            <a href="/about">о нас</a>
+            <a href="/maps">карты</a>
+            <a href="/products">продукты</a>
+            <a href="/best">лучшее</a>
+          </nav>
         </div>
-        <nav>
-          <a href="/">главная</a>
-          <a href="/about">о нас</a>
-          <a href="/maps">карты</a>
-          <a href="/products">продукты</a>
-          <a href="/best">лучшее</a>
-        </nav>
       </div>
     </div>
   </div>
