43564367005df193c6568b9e34e0036af3cfb41f
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-14 15:59:56 +0500

    feat: quickfix

--- App/Views/parts/header.php
+++ App/Views/parts/header.php
@@ -1,18 +1,23 @@
 <header>
-  <div class="logo-image p-5">
-    <h1 class="text-white">Hastle.</h1>
-  </div>
-  <div class="info-box bg-warning"> 
-    <div class="row">
-      <div class="col">быстрый контакт: 8 747-233-36-00</div>
-      <div class="col">Улица: Атмосферная 2/5, офис 216</div>
+  <div class="box-logo-image p-5">
+    <div class="logo align-items-start">
+      <h1 class="text-white">Hastle.</h1>
     </div>
-    <nav>
-      <a href="/">главная</a>
-      <a href="/about">о нас</a>
-      <a href="/maps">карты</a>
-      <a href="/products">продукты</a>
-      <a href="/best">лучшее</a>
-    </nav>
+    <div class="header-content align-items-end text-white">
+      <div class="info-box"> 
+        <div class="row">
+          <div class="col">быстрый контакт: 8 747-233-36-00</div>
+          <div class="col">Улица: Атмосферная 2/5, офис 216</div>
+        </div>
+        <nav>
+          <a href="/">главная</a>
+          <a href="/about">о нас</a>
+          <a href="/maps">карты</a>
+          <a href="/products">продукты</a>
+          <a href="/best">лучшее</a>
+        </nav>
+      </div>
+    </div>
   </div>
+  
 </header>
