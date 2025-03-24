177a0985a1b88450e0958f35caff816d5e87837c
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-25 00:03:26 +0500

    feat: css, quikcfix

--- App/Views/home.php
+++ App/Views/home.php
@@ -17,11 +17,13 @@
     <div class="position-relative bg-dark p-1 mb-3 text-white custom-img-size">    
 
       <div class="position-absolute py-3 px-5 top-0 start-0 w-100 h-100 d-flex flex-column justify-content-between">
-        <div class="">
+        <div class="custom-fone-grey p-2">
           <h2>Бесплатный тренинг по бродкастингу</h2>
           <p>Запись будет с 9 по 15 число, потому что места ограничены. Пробные бесплатные 3 урока. Секретные методики и ...</p>
         </div>
-        <div>перейти</div>
+        <div>
+          <button type="button" class="btn btn-custom-orange">Перейти</button>
+        </div>
       </div>
 
       <img src="images/dd.png" alt="imagen" class="w-100">
@@ -29,13 +31,17 @@
   </div>
 </div>
 
-
 <div class="row">
   <div class="col">
-    <div class="bg-success p-2 mb-3 text-white">
+    <div class="py-3 px-5 mb-3">
       <h3>Новые события в городе - здесь </h3>
     </div>
   </div>
 </div>
- <!-- грид плитки событий -->
+
+<div class="row">
+  <div class="col">
+    preparing
+  </div>
+</div>
 
