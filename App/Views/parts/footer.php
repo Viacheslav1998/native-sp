961a97455bfd11c6f57dd4a0ca4fde8f5b73f518
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-29 02:18:08 +0500

    feat: footer

--- App/Views/parts/footer.php
+++ App/Views/parts/footer.php
@@ -1,5 +1,38 @@
 <footer>
  <div class="bg-dark text-white p-3">
-  <h3>тут какой то контент</h3>
+  <div class="row">
+    <div class="col m-1">
+      <div class="logo-mini">
+        <h6>Hastle.</h6>
+      </div>
+      <div>
+        <p>Выбранный город:</p>
+        <p>Караганда</p>
+      </div>
+    </div>
+    <div class="col m-1">
+      <div class="logo-mini">
+        <h6>быстрая навигация:</h6>
+      </div>
+      <div>
+        <ul>
+          <li><a href="">Ночные события</a></li>
+          <li><a href="">Дневные события</a></li>
+          <li><a href="">Специальные события</a></li>
+          <li><a href="">события</a></li>
+          <li><a href="">события</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col m-1">
+      <div class="logo-mini">
+        <h6>Тут что то</h6>
+      </div>
+      <div>
+        <p>какой то контент - лого ?</p>
+        <p>можно и 4 блок</p>
+      </div>
+    </div>
+  </div>
  </div>
 </footer>
