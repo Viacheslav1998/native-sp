d92eecec26b465d77ea696a44a511f69dbc38379
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-30 19:04:15 +0500

    feat: content admin

--- App/Views/admin/dashboard.php
+++ App/Views/admin/dashboard.php
@@ -1,1 +1,8 @@
-<h1>Админка - здарова админ</h1>
+<div class="custom-silver my-4 p-4 custom-height">
+  <div class="row">
+    <div class="col">
+      <h2>Добро пожаловать</h2>
+      <p>Здесь ты можешь просто управлять пока самым элементарным</p>
+    </div>
+  </div>
+</div>
--- App/Views/admin/parts/header.php
+++ App/Views/admin/parts/header.php
@@ -3,7 +3,7 @@
     <b>ADM-HUSTLE.</b>
     <img class="size-head" src="images/wp.jpg">
   </div>
-  <div class="menu">
+  <div class="menu custom-silver">
     <div class="row p-3">
       <div class="col text-center">
         <a href="#" class="btn btn-outline-secondary fs-5 w-100 py-4">Главная</a>
