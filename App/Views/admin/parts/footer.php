50cbd9965bae0dc129b4c1a60d197b1266bdbb34
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-30 18:53:34 +0500

    feat: footer admin

--- App/Views/admin/parts/footer.php
+++ App/Views/admin/parts/footer.php
@@ -1,1 +1,10 @@
-<h2>подвал админки</h2>
+<footer>
+  <div class="bg-dark pt-2 pb-1 px-2">
+    <div class="row">
+      <div class="col text-white">
+        <h5>Самая обычная админ панель</h5>
+      </div>
+    </div>
+  </div>
+</footer>
+
