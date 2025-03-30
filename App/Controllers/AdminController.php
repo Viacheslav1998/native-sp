c22b95c32888b689b888fddbf9bc35b55985789f
theeKirat1984 <amazingSevenfix@gmail.com>
2025-03-30 18:16:20 +0500

    feat: preparing admin panel template

--- App/Controllers/AdminController.php
+++ App/Controllers/AdminController.php
@@ -6,6 +6,7 @@ class AdminController extends \Core\Controller
 {
   // construct auth
 
+
   public function dashboard()
   {
     return $this->render('admin/dashboard', ['title' => 'Добро пожаловать в админку'], 'admin');
--- App/Views/admin/parts/header.php
+++ App/Views/admin/parts/header.php
@@ -1,1 +1,20 @@
-<h1>шапка админки</h1>
+<header>
+  <div class="row">
+    <b>ADM-HUSTLE.</b>
+    <img class="size-head" src="images/wp.jpg">
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="">
+        <h1>asd</h1>
+      </div>
+    </div>
+    <div class="col">
+      asd
+    </div>
+  </div>
+</header>
+
+<style>
+
+</style>
