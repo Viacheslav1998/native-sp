4b9e30f0f32849e5f9b6969ff98ddc6e4bfea913
theeKirat1984 <amazingSevenfix@gmail.com>
2025-04-02 01:20:35 +0500

    feat: files, events, persons, items

--- App/Views/admin/parts/footer.php
+++ App/Views/admin/parts/footer.php
@@ -2,11 +2,11 @@
   <div class="bg-dark pt-2 pb-1 px-2">
     <div class="row">
       <div class="col text-white">
-        <h5>Самая обычная админ панель</h5>
+        <h6>ADM.HASTLE.</h6>
         <a href="/" class="btn btn-outline-warning">ссылка на основной сайт</a>
       </div>
       <div class="col">
-        <p class="text-white">Быстрый инструмент</p>
+        <h6 class="text-white">faster tools</h6>
         <a href="https://getbootstrap.com/docs/5.0/getting-started/introduction/" class="btn btn-outline-primary">Bootstrap 5</a>
       </div>
     </div>
