f75ada6ef0970804dd92db8cfe4eacd216683f27
theeKirat1984 <amazingSevenfix@gmail.com>
2025-04-03 17:12:01 +0500

    feat: p2 preparing form event

--- App/Views/admin/create.php
+++ App/Views/admin/create.php
@@ -2,10 +2,36 @@
   <p class="fs-2">Добавить событие</p>
   <div class="row">
     <div class="col">
-    <div class="mb-3">
-      <label for="exampleFormControlInput1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-    </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Название события</label>
+        <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example">
+      </div>
+      <br>
+      <div>
+        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+          <option selected>Выбор типа события:</option>
+          <option value="1">Дневное</option>
+          <option value="2">Ночное</option>
+          <option value="3">...</option>
+        </select>
+      </div>
+      <br>
+      <div>
+        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+          <option selected>Выбор Региона</option>
+          <option value="1">Караганда</option>
+          <option value="2">Астана</option>
+          <option value="3">Алматы</option>
+        </select>
+      </div>
+      <div>
+        <label for="formFileLg" class="form-label">Large file input example</label>
+        <input class="form-control form-control-lg" id="formFileLg" type="file">
+      </div>
     </div>
   </div>
 </div>
