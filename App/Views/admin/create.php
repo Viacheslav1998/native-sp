bc0bd9a632e2b0340682a431546040b5a946db69
theeKirat1984 <amazingSevenfix@gmail.com>
2025-04-03 18:56:03 +0500

    feat: event form compleate front

--- App/Views/admin/create.php
+++ App/Views/admin/create.php
@@ -5,36 +5,35 @@
       <div class="col">
         <div class="mb-3">
           <label for="name" name="name" class="form-label text-secondary">Название события</label>
-          <input class="form-control form-control-lg cs-grey" type="text" placeholder="напиши название события" aria-label=".form-control-lg" required>
-          <div class="invalid-feedback">Не заполнен!</div>
+          <input class="form-control form-control-lg cs-grey " type="text" placeholder="напиши название события" aria-label=".form-control-lg" required>
         </div>
         <div class="mb-3">
           <label for="desk_mini" name="desk_mini" class="form-label text-secondary">Короткое описание</label>
-          <input class="form-control form-control-lg cs-grey" type="text" placeholder="короткое описание" aria-label=".form-control-lg">
+          <input class="form-control form-control-lg cs-grey" type="text" placeholder="короткое описание" aria-label=".form-control-lg" required>
         </div>
         <div>
           <label for="event_type" class="form-label text-secondary">Выбери Событие</label>
-          <select name="event_type" class="form-select form-select-lg mb-3 cs-grey" aria-label=".form-select-lg">
-            <option selected>Выбор типа события:</option>
-            <option value="1">Дневное</option>
-            <option value="2">Ночное</option>
-            <option value="3">...</option>
+          <select name="event_type" class="form-select form-select-lg mb-3 cs-grey" aria-label=".form-select-lg" required>
+            <option value="" selected disabled>Выбор типа события:</option>
+            <option value="Дневное">Дневное</option>
+            <option value="Ночное">Ночное</option>
+            <option value="Городское">Городское</option>
           </select>
         </div>
         <div>
           <label for="regional" class="form-label text-secondary">Выбери Регион</label>
-          <select name="regional" class="form-select form-select-lg mb-3 cs-grey" aria-label=".form-select-lg">
-            <option selected>Выбор Региона</option>
-            <option value="1">Караганда</option>
-            <option value="2">Астана</option>
-            <option value="3">Алматы</option>
+          <select name="regional" class="form-select form-select-lg mb-3 cs-grey" aria-label=".form-select-lg" required>
+            <option value="" selected disabled>Выбор Региона</option>
+            <option value="Караганда">Караганда</option>
+            <option value="Астана">Астана</option>
+            <option value="Алматы">Алматы</option>
           </select>
         </div>
         <div>
         <label for="fullDesk" class="form-label text-secondary">Полное описание</label>
         <br>
         <div class="form-floating">
-          <textarea rows="8" style="height: 100%;" name="fullDesk" class="form-control cs-grey" placeholder="Полное описание" id="fullDesk"></textarea>
+          <textarea rows="8" style="height: 100%;" name="fullDesk" class="form-control cs-grey" placeholder="Полное описание" id="fullDesk" required></textarea>
           <label for="fullDesk">Полное описание твоего события...</label>
         </div>
         <br>
