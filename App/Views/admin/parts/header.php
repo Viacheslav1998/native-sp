463b919a4c2e1f4e18c7008710f4296b055c2674
theeKirat1984 <amazingSevenfix@gmail.com>
2025-04-05 19:50:10 +0500

    quickfix

--- App/Views/admin/parts/header.php
+++ App/Views/admin/parts/header.php
@@ -3,25 +3,25 @@
     <b>ADM-HUSTLE.</b>
     <img class="size size-head" src="/images/wp.jpg">
   </div>
-  <div class="menu custom-silver">
+  <div class="menu">
     <div class="row p-3">
-      <div class="col">
-        <a href="/a4min/dashboard" class="d-flex align-items-center justify-content-center btn btn-outline-secondary w-100 h-100 py-4">Главная</a>
+      <div class="col my-2 text-center">
+        <a href="/a4min/dashboard" class=" d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Главная</a>
       </div>
-      <div class="col text-center">
-        <a href="/a4min/add" class="d-flex align-items-center justify-content-center btn btn-outline-secondary w-100 h-100 py-4">Добавить простое событие</a>
+      <div class="col my-2 text-center">
+        <a href="/a4min/add" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Добавить простое событие</a>
       </div>
-      <div class="col text-center">
-        <a href="/a4min/add" class="d-flex align-items-center justify-content-center btn btn-outline-secondary w-100 h-100 py-4">Добавить главное событие</a>
+      <div class="col my-2 text-center">
+        <a href="/a4min/add" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Добавить главное событие</a>
       </div>
-      <div class="col text-center">
-        <a href="/a4min/events" class="d-flex align-items-center justify-content-center btn btn-outline-secondary w-100 h-100 py-4">Управление простыми событиями</a>
+      <div class="col my-2 text-center">
+        <a href="/a4min/events" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Управление простыми событиями</a>
       </div>
-      <div class="col text-center">
-        <a href="/a4min/events" class="d-flex align-items-center justify-content-center btn btn-outline-secondary w-100 h-100 py-4">Управление главными событиями</a>
+      <div class="col my-2 text-center">
+        <a href="/a4min/events" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Управление главными событиями</a>
       </div>
-      <div class="col text-center">
-        <a href="a4min/persons" class="d-flex align-items-center justify-content-center btn btn-outline-secondary w-100 h-100 py-4">Управление пользователями</a>
+      <div class="col my-2 text-center">
+        <a href="a4min/persons" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Управление пользователями</a>
       </div>
     </div>
   </div>
