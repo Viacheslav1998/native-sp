d4daad62fd873b70ed0cc1f0f4284289c7882580
theeKirat1984 <amazingSevenfix@gmail.com>
2025-04-08 19:27:02 +0500

    quickfix, preparing, test

---
+++ App/Models/EventModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+class EventModel extends Model
+{
+    public function getEvents()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM events");
+        return $stmt->fetchAll();
+    }
+}
