a22076a3c07df6ad42258152442729849d33d92a
theeKirat1984 <amazingSevenfix@gmail.com>
2025-04-10 17:58:41 +0500

    quickfix

--- config/example.database.php
+++ config/example.database.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * this is a demonstration file that to make it work you need ->
- * delete the point (.) extensions in the file for the configuration to work
+ * delete the(example.) extensions in the file for the configuration to work
  * and don't forget your server settings
  */
    
