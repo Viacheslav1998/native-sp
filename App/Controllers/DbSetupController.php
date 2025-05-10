1a740111a2dc38ac90f7f1e0eef6a9968adf80ef
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-10 16:44:23 +0500

    feat: fix, doc-commments

--- App/Controllers/DbSetupController.php
+++ App/Controllers/DbSetupController.php
@@ -5,17 +5,26 @@ namespace App\Controllers;
 use Core\Migration;
 use Core\Model;
 
+/**
+ * Checking the status of the production server 
+ * generate/create DB/Tables/Columns
+ */
 class DbSetupController {
 
+    /**
+     * generate/create/update/rename Table - Column.
+     */
     public function generate()
     {
       try {
           $pdo = Model::staticPDO();
           $migrations = new Migration($pdo);
           $migrations->run();
+          http_response_code(201);
           echo json_encode(['Success' => ' Успех! Создано!']);
       } catch (\PDOException $e) {
           throw new Exception("возникли проблемы с базой данных");
+          httr_response_code(500);
           error_log('Error: ', $e->getMessage());
       }
     }
