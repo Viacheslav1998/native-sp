47b8e1d1520f3580ebc94e88fc572e52e6f96267
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-17 00:02:16 +0500

    feat: helper auth

---
+++ App/Helpers/Auth.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * verification and setting of rights user
+ * access control
+ * session destruction
+ */
+class Auth
+{
+    public static function check(): bool
+    {
+        return isset($_SESSION['user']);
+    }
+
+    public static function user(): ?array
+    {
+        return $_SESSION['user'] ?? null;
+    }
+
+    public static function isAdmin(): bool
+    {
+        return self::check() && ($_SESSION['user']['role'] ?? null) === 'admin';
+    }
+
+    public static function requireAdmin()
+    {
+        if(!self::isAdmin()) {
+            header("Location: /");
+            exit;
+        }
+    }
+
+    public static function login(array $user): void
+    { 
+        $_SESSION['user'] = $user;
+    }
+
+    public static function logout(): void
+    {
+        unset($_SESSION['user']);
+    }
+}
