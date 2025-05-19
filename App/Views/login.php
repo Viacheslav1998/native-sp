a9bbfcf088c95540ee3070cd665723dea900d760
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-19 19:00:20 +0500

    feat: html login, css, quickfix, finished

--- App/Views/login.php
+++ App/Views/login.php
@@ -1,33 +1,33 @@
-<!-- <div class="row">
-  <div class="col">
-    <h1>Страница регистрации</h1>
-  </div>
-</div> -->
+
 <div class="fone">
+  <div class="decor"></div>
   <div class="row">
-    <div class="col text-center text-white pt-3 pb-2">
+    <div class="col text-center text-white pt-4">
       <h1>Страница для входа в систему.</h1>
       <p>Давай скорее - твои события уже ждут!</p>
     </div>
   </div>
-  <div class="wrapper-form-login d-flex justify-content-center">
-    <div class="form-login p-5 mb-4">
+  <div class="wrapper-form-login d-flex justify-content-center pt-4 pb-5">
+    <div class="form-login p-5 mb-4 shadow">
       <form id="form-login" enctype="multipart/form-data">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class="form-group p-2 text-success fw-bold">
+          <label for="exampleInputEmail1" class="fs-5">Нужна твоя почта</label>
+          <input type="email" class="form-control px-4 text-danger" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Вводи свою почту">
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <div class="form-group p-2 text-success fw-bold">
+          <label for="exampleInputPassword1" class="fs-5">И Пароль</label>
+          <input type="password" class="form-control px-4 text-danger" id="exampleInputPassword1" placeholder="Вводи свой пароль">
         </div>
-        <div class="form-group form-check">
+        <div class="form-group form-check m-2 py-2">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <label class="form-check-label text-black" for="exampleCheck1">Запомнить меня</label>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-danger m-2">Зайти</button>
       </form>
     </div>
+  </div>
+  <div class="text-white text-center mb-5 fs-4">
+    Если тебя ещё нет в системе то перейди и зарегистрируйся<br>
+    <a href="/register" class="btn btn-warning m-2 fs-5">Регистрация</a>
   </div>
 </div>
