ac9ad7ec41c61b27569c21ae32aad7932b942395
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-19 19:18:48 +0500

    feat: page register

---
+++ App/Views/register.php
@@ -0,0 +1,33 @@
+
+<div class="fone">
+  <div class="decor"></div>
+  <div class="row">
+    <div class="col text-center text-white pt-4">
+      <h1>Страница для Регистрации.</h1>
+      <p>В это системе ты сможешь реализовать свои события!</p>
+    </div>
+  </div>
+  <div class="wrapper-form-login d-flex justify-content-center pt-4 pb-5">
+    <div class="form-login p-5 mb-4 shadow">
+      <form id="form-login" enctype="multipart/form-data">
+        <div class="form-group p-2 text-success fw-bold">
+          <label for="exampleInputEmail1" class="fs-5">Нужна твоя почта</label>
+          <input type="email" class="form-control px-4 text-danger" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Вводи свою почту">
+        </div>
+        <div class="form-group p-2 text-success fw-bold">
+          <label for="exampleInputPassword1" class="fs-5">И Пароль</label>
+          <input type="password" class="form-control px-4 text-danger" id="exampleInputPassword1" placeholder="Вводи свой пароль">
+        </div>
+        <div class="form-group form-check m-2 py-2">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label text-black" for="exampleCheck1">Запомнить меня</label>
+        </div>
+        <button type="submit" class="btn btn-danger m-2">Зайти</button>
+      </form>
+    </div>
+  </div>
+  <div class="text-white text-center mb-5 fs-4">
+    Если ты уже есть в системе просто заходи<br>
+    <a href="/login" class="btn btn-warning m-2 fs-5">Вход</a>
+  </div>
+</div>
