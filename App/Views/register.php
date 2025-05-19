b186acb64cff85e2595ee44ea8f962bb322625d0
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-20 01:48:56 +0500

    feat: form registration

--- App/Views/register.php
+++ App/Views/register.php
@@ -1,28 +1,55 @@
 
-<div class="fone">
+<div class="fone-r">
   <div class="decor"></div>
   <div class="row">
     <div class="col text-center text-white pt-4">
       <h1>Страница для Регистрации.</h1>
-      <p>В это системе ты сможешь реализовать свои события!</p>
+      <p>В этой системе ты сможешь реализовать свои события!</p>
+      <i>Люди смогут, увидеть и понять кто создал событие, с какого оно города, кто его инициатор. <br> Но переживать не нужно, ведь очень важные конфиденциальные данные будут скрыты.</i>
     </div>
   </div>
-  <div class="wrapper-form-login d-flex justify-content-center pt-4 pb-5">
-    <div class="form-login p-5 mb-4 shadow">
-      <form id="form-login" enctype="multipart/form-data">
-        <div class="form-group p-2 text-success fw-bold">
-          <label for="exampleInputEmail1" class="fs-5">Нужна твоя почта</label>
-          <input type="email" class="form-control px-4 text-danger" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Вводи свою почту">
+  <div class="wrapper-form-register d-flex justify-content-center pt-4 pb-5">
+    <div class="form-register p-5 m-4 shadow">
+      <form id="form-register" enctype="multipart/form-data">
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="inputName" class="fs-5">Твое имя</label>
+          <input type="text" class="form-control px-4 text-primary" id="inputName" placeholder="Введи свое имя">
         </div>
-        <div class="form-group p-2 text-success fw-bold">
-          <label for="exampleInputPassword1" class="fs-5">И Пароль</label>
-          <input type="password" class="form-control px-4 text-danger" id="exampleInputPassword1" placeholder="Вводи свой пароль">
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="inputFirstName" class="fs-5">Твоя фамилия</label>
+          <input type="text" class="form-control px-4 text-primary" id="inputFirstName" placeholder="Введи свою фамилию">
         </div>
-        <div class="form-group form-check m-2 py-2">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label text-black" for="exampleCheck1">Запомнить меня</label>
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="inputEmail" class="fs-5">Твоя почта</label>
+          <input type="email" class="form-control px-4 text-primary" id="inputEmail" placeholder="Введи свою почту">
         </div>
-        <button type="submit" class="btn btn-danger m-2">Зайти</button>
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="selectTown" class="fs-5">Твой город</label>
+          <select class="form-control fs-5" id="selectTown">
+            <option>Караганда</option>
+            <option>Алмата</option>
+            <option>Астана</option>
+            <option>Темиртау</option>
+            <option>Атырау</option>
+          </select>
+        </div>
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="inputPassword" class="fs-5">Твой Пароль</label>
+          <input type="password" class="form-control px-4 text-primary" id="inputPassword" placeholder="Введи свой пароль">
+        </div>
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="inputPasswordAgain" class="fs-5">Еще раз пароль</label>
+          <input type="password" class="form-control px-4 text-primary" id="inputPasswordAgain" placeholder="Нужно ввести пароль еще раз">
+        </div>
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="inputPhone" class="fs-5">Твой телефон</label>
+          <input type="text" class="form-control px-4 text-primary" id="exampleInputPassword1" placeholder="Нужен твой номер телефона">
+        </div>
+        <div class="form-group p-2 text-dark fw-bold">
+          <label for="images">Если нужно можно выбрать фотку или аву</label>
+          <input type="file" class="form-control-file" id="images">
+        </div>
+        <button type="submit" class="btn btn-dark m-2">Регистрация</button>
       </form>
     </div>
   </div>
