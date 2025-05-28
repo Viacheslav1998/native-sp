af4de1d2784df75d4cc356cad2056135407bc984
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-28 11:44:32 +0500

    feat: header admin

--- App/Views/admin/parts/header.php
+++ App/Views/admin/parts/header.php
@@ -5,22 +5,22 @@
   </div>
   <div class="menu custom-silver">
     <div class="row p-3">
-      <div class="col my-2 text-center">
+      <div class="col-xl-4 col-sm-6 my-2 text-center">
         <a href="/a4min/dashboard" class=" d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Главная</a>
       </div>
-      <div class="col my-2 text-center">
+      <div class="col-xl-4 col-sm-6 my-2 text-center">
         <a href="/a4min/form-regular-event" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Добавить простое событие</a>
       </div>
-      <div class="col my-2 text-center">
+      <div class="col-xl-4 col-sm-6 my-2 text-center">
         <a href="/a4min/form-main-event" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Добавить главное событие</a>
       </div>
-      <div class="col my-2 text-center">
+      <div class="col-xl-4 col-sm-6 my-2 text-center">
         <a href="/a4min/events" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Управление простыми событиями</a>
       </div>
-      <div class="col my-2 text-center">
+      <div class="col-xl-4 col-sm-6 my-2 text-center">
         <a href="/a4min/main-events" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Управление главными событиями</a>
       </div>
-      <div class="col my-2 text-center">
+      <div class="col-xl-4 col-sm-6 my-2 text-center">
         <a href="/a4min/persons" class="d-flex align-items-center justify-content-center btn btn-outline-dark w-100 h-100 py-4">Управление пользователями</a>
       </div>
     </div>
