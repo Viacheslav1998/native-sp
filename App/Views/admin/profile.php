a02a7e429574e063478711f8ed57028d8d4c06e7
theeKirat1984 <amazingSevenfix@gmail.com>
2025-05-29 05:23:01 +0500

    feat: profile

--- App/Views/admin/profile.php
+++ App/Views/admin/profile.php
@@ -1,13 +1,50 @@
 <div class="custom-silver my-4 p-4 custom-height">
   <div class="row">
-    <div class="col">
-      <h2>Здравствуй ! пользователь</h2>
-    </div>
-    <div class="">
-      <img src="/images/foneb.jpg" alt="..." class="img-thumbnail">
+    <div class="col bg-dark mb-5 text-white">
+      <h4 class="px-3 pt-2 mx-2">Профиль</h4>
     </div>
   </div>
-<div class="row mb-4">
-   фото профиля 
-   поменять
+  <div class="row">
+    <div class="col-md-4 text-center mb-5 ">
+      <div class="person-imagen">
+        <img src="/images/foneb.jpg" alt="Фото профиля" class="img-thumbnail">
+        <p>изменить картинку</p>
+      </div>
+    </div>
+    <div class="col">
+      <h3>Привет ! Пользователь</h3>
+      <p style="font-weight: 100; font-size: 24px;" class="text-dark"> здесь ты сможешь сменить профиль</p>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
 </div>
