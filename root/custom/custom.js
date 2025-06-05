6102280092f71222304a54c23664be83bbc582e6
theeKirat1984 <amazingSevenfix@gmail.com>
2025-06-06 00:36:24 +0500

    feat: login start js, quickfix location

--- root/custom/custom.js
+++ root/custom/custom.js
@@ -1,1 +1,2 @@
 import register from './custom_components/register.js';
+import login from './custom_components/login.js';
---
+++ root/custom/custom_components/login.js
@@ -0,0 +1,12 @@
+/**
+ * only login page
+ */
+
+// import modal from './modalHandler.js';
+
+if(window.location.pathname === '/login') {
+
+  alert('Вижу только в логин пэйдж');
+}
+
+export default {}
