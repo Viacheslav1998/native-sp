457677cf0d4d9aa82b85765d1ad13ba58243ec67
theeKirat1984 <amazingSevenfix@gmail.com>
2025-08-24 04:36:58 +0500

    feat: preparing events, products

--- App/Controllers/EventController.php
+++ App/Controllers/EventController.php
@@ -15,4 +15,25 @@ class EventResource extends \Core\Controller
     {
         echo "this is work a wonderfull - test message";
     }
+
+    /**
+     * events filter
+     * town | events [night, regular ...]
+     */
+    public function sortEvent(string $town, string $event = 'regular')
+    {
+
+        // swith case ??? or or or
+
+        if(!$town || !$event) {
+            return getDefault();
+        } 
+
+        return getFilter($town, $event);
+
+    }
+
+    // 1 event id \ name \ filter \ country \  ...
+    // sortEvent = Astana/[events]
+    // filter universan = sortEvent / night/[events] ... 
 }
---
+++ App/Controllers/ProductsController.php
@@ -0,0 +1,4 @@
+<?php
+
+// class products -> create DB - model product - 
+// add - fun short products
