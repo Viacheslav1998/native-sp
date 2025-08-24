cf98347951645ea80ae742b664529b5b1345692c
theeKirat1984 <amazingSevenfix@gmail.com>
2025-08-24 19:32:35 +0500

    feat: response helper

--- App/Controllers/EventController.php
+++ App/Controllers/EventController.php
@@ -1,26 +1,47 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Helpers\Request;
+use App\Helpers\Response;
 use App\Models\Event;
 
 class EventResource extends \Core\Controller
 {
-    public function getEvents()
+    public function getEvent($id): array
+    {
+        try {
+            $event = $this->eventModel->getItem($id);
+
+            
+
+            echo json_encode([
+                'status' => 'success',
+                'code' => 200,
+                'data' => $event,
+            ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        
+        } catch (Exception $e) {
+            http_response_code(500);
+            header('Content-type: application/json; charset=utf-8');
+            
+            echo json_encode([
+                'status' => 'error',
+                'message' => 'Серверная ошибка',
+            ], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    public function getEvents(): array
     {
         return true;
     }
 
-    public function saveEvent()
-    {
-        echo "this is work a wonderfull - test message";
-    }
 
     /**
      * events filter
      * town | events [night, regular ...]
      */
-    public function sortEvent(string $town, string $event = 'regular')
+    public function sortEvent(string $town, string $event = 'regular'): array
     {
 
         // swith case ??? or or or
