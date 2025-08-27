4766f73f3834fba915c46692cfc6234d9d05c312
theeKirat1984 <amazingSevenfix@gmail.com>
2025-08-27 23:35:54 +0500

    feat: backup

---
+++ App/Services/DatabaseBackup.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Services;
+
+class DatabaseBackup
+{
+    protected string $backupFile;
+    protected string $flagFile;
+
+    public function __construct()
+    {
+        $this->backupFile = __DIR__ . '../../db_backup.sql';
+        $this->flagFile = __DIR__ . '../../.backup_done';
+    }
+
+    public function backup(): void
+    {
+        $firstRun = !file_exists($this->flagFile);
+
+        if ($firstRun || $this->needsBackup()) {
+          $this->doBackup();
+          file_put_contents($this->flagFile, date('Y-m-d H:i:s'));
+          echo "Backup completed! \n";
+        } else {
+          echo "Backup";
+        }
+    }
+
+    protected function needsBackup(): bool
+    {
+        $last = file_get_contents($this->flagFile);
+        return (time() - strtotime($last)) > 3600;
+    }
+
+    protected function doBackup(): void
+    {
+        $dbUser = 'user';
+        $dbPass = '1914';
+        $dbName = 'hastle';
+
+        exec("mysqldump -u {$dbUser} -p{$dbPass} {$dbName} > {$this->backupFile}");
+    }
+}
---
+++ bin/backup.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Services\DatabaseBackup;
+
+$backup = new DatabaseBackup();
+$backup->backup(); 
